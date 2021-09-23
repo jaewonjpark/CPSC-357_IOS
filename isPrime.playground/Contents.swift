bfda854af6d5a64f1631cb245778fbf266e69e64
Jaewon Park <jaepark@chapman.edu>
2021-09-22 20:17:28 -0700

    isPrime update

---
+++ isPrime.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//Prime Number (can only be divided by itself or 1)
+func isPrime(n: Int) -> Bool {
+    // has to be greater than 1
+    if n <= 1 {
+        return false
+    }
+    //2 and 3 are always true
+    if n <= 3 {
+        return true
+    }
+
+    var num = 2
+    while num*num <= n {
+        if n % num == 0 {
+            return false
+        }
+        num += 1
+    }
+    return true
+}
+
+isPrime(n: 2) // true
+isPrime(n: 3) // true
+isPrime(n: 10) // false
+isPrime(n: 13) // true
+
+/*:
+ function isPrime
+ 
